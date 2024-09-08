89ab4147a61c562660751c08466589b1d0b57388
awwpotato <awwpotato@noreply.codeberg.org>
2024-09-07 17:07:44 -0700

    chore: user.js comments

--- user.js
+++ user.js
@@ -1,23 +1,23 @@
 // userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// Fill SVG Color
+// fill svg color
 user_pref("svg.context-properties.content.enabled", true);
 
-// CSS's `:has()` selector 
+// enable :has selector
 user_pref("layout.css.has-selector.enabled", true);
 
-// Integrated calculator at urlbar
+// integrated calculator at urlbar
 user_pref("browser.urlbar.suggest.calculator", true);
 
-// Integrated unit convertor at urlbar
+// integrated unit convertor at urlbar
 user_pref("browser.urlbar.unitConversion.enabled", true);
 
-// Trim  URL
+// trim url
 user_pref("browser.urlbar.trimHttps", true);
 user_pref("browser.urlbar.trimURLs", true);
 
-// GTK rounded corners
+// gtk rounded corners
 user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
 
 // show compact mode
@@ -26,7 +26,6 @@ user_pref("browser.compactmode.show", true);
 // fix sidebar tab drag on linux
 user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
 
-// option for transparent tab bg (disabled to due issues on some websites)
 user_pref("browser.tabs.allow_transparent_browser", true);
 
 // uidensity -> compact
