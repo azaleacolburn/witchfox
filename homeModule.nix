4f69d74fa0082d9a3777024027520a0962576c0f
awwpotato <awwpotato@voidq.com>
2025-07-04 12:42:18 -0700

    fix(nix): hopefully resolve inf rec issues 2

--- homeModule.nix
+++ homeModule.nix
@@ -58,81 +58,83 @@ in
   });
 
   config = lib.mkMerge (
-    builtins.concatMap (
+    lib.forEach browsers (
       name:
       let
         cfg = config.programs.${name}.potatofox;
       in
-      lib.optionals cfg.enable (
-        lib.mapAttrsToList (n: pcfg: {
-          home.file.".${if name == "librewolf" then "librewolf" else "mozzila/${name}"}/${n}/chrome" = {
+      lib.optionalAttrs cfg.enable {
+        home.file = lib.mapAttrs' (
+          n: pcfg:
+          lib.nameValuePair ".${if name == "librewolf" then "librewolf" else "mozzila/${name}"}/${n}/chrome" {
             source = ./chrome;
             recursive = true;
-          };
-          programs.${name}.profiles.${n} = {
-            settings = lib.mkMerge [
-              (lib.mkIf pcfg.prefs.enable {
-                "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-                "browser.tabs.allow_transparent_browser" = true;
-                "svg.context-properties.content.enabled" = true;
-                "layout.css.has-selector.enabled" = true;
-                "browser.urlbar.suggest.calculator" = true;
-                "browser.urlbar.unitConversion.enabled" = true;
-                "browser.urlbar.trimHttps" = true;
-                "browser.urlbar.trimURLs" = true;
-                "widget.gtk.rounded-bottom-corners.enabled" = true;
-                "browser.compactmode.show" = true;
-                "widget.gtk.ignore-bogus-leave-notify" = 1;
-                "browser.uidensity" = 1;
-              })
-              (lib.mapAttrs' (name: value: lib.nameValuePair "uc.tweak.${name}" value) pcfg.tweaks)
-            ];
-            extensions = {
-              packages =
-                let
-                  inherit (pkgs.stdenv.hostPlatform) system;
-                in
-                with inputs.nur.legacyPackages.${system}.repos.rycee;
-                lib.mkIf pcfg.extensions.enable [
-                  sidebery
-                  userchrome-toggle-extended
-                ];
-              settings."userchrome-toggle-extended@n2ezr.ru".settings =
-                lib.mkIf pcfg.extensions.userchrome-toggle.configure
-                  {
-                    initialized = true;
-                    settingsVer = "2";
+          }
+        ) cfg.profiles;
 
-                    allowMultiple = true;
-                    closePopup = true;
-                    useLastWindowToggles = false;
-                    keepToggles = false;
+        programs.${name}.profiles = lib.mapAttrs (n: pcfg: {
+          settings = lib.mkMerge [
+            (lib.mkIf pcfg.prefs.enable {
+              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+              "browser.tabs.allow_transparent_browser" = true;
+              "svg.context-properties.content.enabled" = true;
+              "layout.css.has-selector.enabled" = true;
+              "browser.urlbar.suggest.calculator" = true;
+              "browser.urlbar.unitConversion.enabled" = true;
+              "browser.urlbar.trimHttps" = true;
+              "browser.urlbar.trimURLs" = true;
+              "widget.gtk.rounded-bottom-corners.enabled" = true;
+              "browser.compactmode.show" = true;
+              "widget.gtk.ignore-bogus-leave-notify" = 1;
+              "browser.uidensity" = 1;
+            })
+            (lib.mapAttrs' (name: value: lib.nameValuePair "uc.tweak.${name}" value) pcfg.tweaks)
+          ];
+          extensions = {
+            packages =
+              let
+                inherit (pkgs.stdenv.hostPlatform) system;
+              in
+              with inputs.nur.legacyPackages.${system}.repos.rycee;
+              lib.mkIf pcfg.extensions.enable [
+                sidebery
+                userchrome-toggle-extended
+              ];
+            settings."userchrome-toggle-extended@n2ezr.ru".settings =
+              lib.mkIf pcfg.extensions.userchrome-toggle.configure
+                {
+                  initialized = true;
+                  settingsVer = "2";
 
-                    toggles = [
-                      {
-                        name = "Style 1";
-                        enabled = true;
-                        prefix = "᠎";
-                        default_state = false;
-                      }
-                      {
-                        name = "Style 2";
-                        enabled = true;
-                        prefix = "​";
-                        default_stat = false;
-                      }
-                      {
-                        name = "Style 3";
-                        enabled = true;
-                        prefix = "‌";
-                        default_state = false;
-                      }
-                    ];
-                  };
-            };
+                  allowMultiple = true;
+                  closePopup = true;
+                  useLastWindowToggles = false;
+                  keepToggles = false;
+
+                  toggles = [
+                    {
+                      name = "Style 1";
+                      enabled = true;
+                      prefix = "᠎";
+                      default_state = false;
+                    }
+                    {
+                      name = "Style 2";
+                      enabled = true;
+                      prefix = "​";
+                      default_stat = false;
+                    }
+                    {
+                      name = "Style 3";
+                      enabled = true;
+                      prefix = "‌";
+                      default_state = false;
+                    }
+                  ];
+                };
           };
-        }) cfg.profiles
-      )
-    ) browsers
+        }) cfg.profiles;
+      }
+    )
   );
 }
